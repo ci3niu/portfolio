de1f3541e5799bd7b1f01131f348abff9f276368
Cieniu <em.cieniek@gmail.com>
2023-02-10 18:49:39 +0100

    added Projects

--- src/Slides/Widescreen/Contact.tsx
+++ src/Slides/Widescreen/Contact.tsx
@@ -1,10 +1,58 @@
+import styled from 'styled-components';
 import TitleScroller from '../../components/TitleScroller/TitleScroller';
 import { StyledContainer } from '../../utils/Container.styled';
+
+import { AiOutlineMail, AiOutlinePhone, AiOutlineGithub, AiOutlineLinkedin } from 'react-icons/ai';
+
+const contactData = [
+	{ type: 'email', value: 'em.cieniek@gmail', icon: <AiOutlineMail fill='#94d0ff' size={60} opacity='0.75' /> },
+	{ type: 'phone', value: '516 505 696', icon: <AiOutlinePhone fill='#94d0ff' size={60} opacity='0.75' /> },
+	{ type: 'github', value: '/ci3niu', icon: <AiOutlineGithub fill='#94d0ff' size={60} opacity='0.75' /> },
+	{ type: 'linkedIn', value: '/m-cieniek', icon: <AiOutlineLinkedin fill='#94d0ff' size={60} opacity='0.75' /> },
+];
+
+const StyledContactContainer = styled.div`
+	position: absolute;
+	top: 30%;
+	left: 10%;
+	display: flex;
+	flex-direction: column;
+`;
+
+const StyledContact = styled.div`
+	display: flex;
+	align-items: center;
+	&:nth-child(2) {
+		margin-left: 3rem;
+	}
+	&:nth-child(3) {
+		margin-left: 6rem;
+	}
+	&:nth-child(4) {
+		margin-left: 9rem;
+	}
+	p {
+		font-family: 'MontserratRegular';
+		font-size: 2rem;
+		padding-left: 1rem;
+	}
+`;
 
 const Contact = () => {
 	return (
 		<StyledContainer>
 			<TitleScroller text='Contact' multiplier={50} toLeft />
+			<StyledContactContainer>
+				{contactData.map((contact) => {
+					const { type, value, icon } = contact;
+					return (
+						<StyledContact key={type}>
+							{icon}
+							<p>{value}</p>
+						</StyledContact>
+					);
+				})}
+			</StyledContactContainer>
 		</StyledContainer>
 	);
 };
--- src/Slides/Widescreen/Projects.tsx
+++ src/Slides/Widescreen/Projects.tsx
@@ -1,13 +1,108 @@
+import styled from 'styled-components';
 import SectionDescription from '../../components/SectionDescription/SectionDescription';
 import TitleScroller from '../../components/TitleScroller/TitleScroller';
 import { StyledContainer } from '../../utils/Container.styled';
+import { AiOutlineGithub, AiOutlineLink } from 'react-icons/ai';
+
+const projectsData = [
+	{
+		name: 'photoBuff',
+		linkGH: 'https://github.com/ci3niu/react-photobuff',
+		linkDemo: 'https://ci3niu.github.io/react-photobuff/',
+		image: '',
+		tools: ['React', 'TypeScript', 'styled-components', 'react-responsive-carousel', 'react-typed'],
+	},
+	{
+		name: 'shopSPA',
+		linkGH: 'https://github.com/ci3niu/react-shop-spa',
+		linkDemo: 'https://ci3niu.github.io/react-shop-spa/',
+		image: '',
+		tools: ['React', 'REST API', 'useReducer', 'ChakraUI'],
+	},
+	{
+		name: 'recipeApp',
+		linkGH: 'https://github.com/ci3niu/vanillaJs-recipe-app',
+		linkDemo: 'https://ci3niu.github.io/vanillaJs-recipe-app/',
+		image: '',
+		tools: ['JavaScript', 'REST API', 'SASS'],
+	},
+];
+
+const StyledProjectsContainer = styled(StyledContainer)`
+	height: 180vh;
+`;
+
+const Asd = styled.div`
+	position: absolute;
+	top: 33%;
+	display: flex;
+	justify-content: space-around;
+	width: 100%;
+`;
+
+const SingleProjectContainer = styled.div`
+	font-family: 'MontserratRegular';
+	align-items: flex-start;
+	display: flex;
+	flex-direction: column;
+	max-width: 20%;
+	a {
+		text-decoration: none;
+		color: ${({ theme }) => theme.colorText};
+	}
+`;
 
 const Projects = () => {
 	return (
-		<StyledContainer>
+		<StyledProjectsContainer>
 			<TitleScroller text='Projects' multiplier={76} />
 			<SectionDescription text={`Recent projects that I've worked on.`} />
-		</StyledContainer>
+			<Asd>
+				{projectsData.map((project) => {
+					const { name, linkGH, linkDemo, image, tools } = project;
+					return (
+						<SingleProjectContainer>
+							<p style={{ marginLeft: '2rem', fontSize: '2rem' }}>{name}</p>
+
+							<p style={{ alignSelf: 'center', fontSize: '3rem', marginBottom: '0' }}>[...]</p>
+							<p style={{ alignSelf: 'center', fontSize: '3rem' }}>[IMAGE HERE]</p>
+
+							<div style={{ display: 'flex', alignItems: 'center', padding: '1rem' }}>
+								<AiOutlineGithub fill='#94d0ff' size={20} opacity='0.75' />
+								<a href={linkGH} target='_blank'>
+									{linkGH}
+								</a>
+							</div>
+
+							<div style={{ display: 'flex', alignItems: 'center', padding: '1rem' }}>
+								<AiOutlineLink fill='#94d0ff' size={20} opacity='0.75' />
+								<a href={linkDemo} target='_blank'>
+									{linkDemo}
+								</a>
+							</div>
+							<div style={{ display: 'flex', flexDirection: 'column', alignSelf: 'center' }}>
+								<p style={{ fontSize: '2rem' }}>Tools used:</p>
+								<div>
+									{tools.map((tool) => (
+										<p
+											style={{
+												margin: '0.5rem',
+												backgroundColor: '#94d0ffBF',
+												padding: '0.6rem 0',
+												borderRadius: '10%',
+												textAlign: 'center',
+											}}
+										>
+											{tool}
+										</p>
+									))}
+								</div>
+							</div>
+						</SingleProjectContainer>
+					);
+				})}
+			</Asd>
+		</StyledProjectsContainer>
 	);
 };
 
